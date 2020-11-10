67fd85e5c7cee35aecbf1dc08c92f3e37c749a6e
zara.alessio <zara.alessio@TAVE-PC02.OSDBSESTO.local>
2020-11-10 13:05:58 +0100

    inizialize i

--- src/github/Github.java
+++ src/github/Github.java
@@ -15,7 +15,7 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i = 0;
+        int i=2;
         while(true){
             System.out.println("hey ciao: " + i);
             i = i+i;
