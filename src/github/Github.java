c79ab64f204f7b06b981af1e8c5dbb17690a6c8c
zara.alessio <zara.alessio@TAVE-PC02.OSDBSESTO.local>
2020-11-10 13:02:59 +0100

    add endless loop

---
+++ src/github/Github.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+/**
+ *
+ * @author zara.alessio
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        while(true){
+            System.out.println("hey ciao: " + i);
+            i = i+i;
+        }
+    }
+    
+}
